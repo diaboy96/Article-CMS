d5f41a386f0065924e9dc41907cacd369e141e8b
diaboy96 <neo6363205@gmail.com>
2019-08-20 15:26:50 +0200

    edit LoginType form + render it to base.html.twig

--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Login;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +14,8 @@ class LoginType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('pass')
-            ->add('email')
-            ->add('hash')
-            ->add('active')
-            ->add('type')
+            ->add('name', TextType::class, ['attr' => ['placeholder' => 'test']])
+            ->add('pass', PasswordType::class)
         ;
     }
 
