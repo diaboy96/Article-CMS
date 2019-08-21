1d4cdd833c0c5eb0f732a41eab8e0617befd14de
diaboy96 <neo6363205@gmail.com>
2019-08-21 12:38:55 +0200

    Prepare for LoginForm handle data

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -11,9 +12,18 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index()
+    public function index(Request $request)
     {
         $login_form = $this->createForm(LoginType::class);
+
+        $login_form->handleRequest($request);
+        if ($login_form->isSubmitted() && $login_form->isValid()) {
+            $form_data = $login_form->getData();
+            $name = $form_data->getName();
+            $pass = $form_data->getPass();
+
+            // todo connect to login API and proceed login
+        }
 
         return $this->render('main/index.html.twig', [
             'login_form' => $login_form->createView()
