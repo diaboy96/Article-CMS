453bd6ecc99c4b27da4f80f7a6ed65bba3e0b5b2
diaboy96 <neo6363205@gmail.com>
2019-08-24 09:19:26 +0200

    Add method login to MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Login;
 use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -14,19 +16,69 @@ class MainController extends AbstractController
      */
     public function index(Request $request)
     {
-        $login_form = $this->createForm(LoginType::class);
+        $session = new Session();
 
-        $login_form->handleRequest($request);
-        if ($login_form->isSubmitted() && $login_form->isValid()) {
-            $form_data = $login_form->getData();
-            $name = $form_data->getName();
-            $pass = $form_data->getPass();
+        $user_id = $session->get('user_id');
+        if (isset($user_id) && !empty($user_id)){
 
-            // todo connect to login API and proceed login
+            return $this->logged_in();
+
+        } else {
+
+            $login_form = $this->createForm(LoginType::class);
+
+            $login_form->handleRequest($request);
+            if ($login_form->isSubmitted() && $login_form->isValid()) {
+                $form_data = $login_form->getData();
+                $name = htmlspecialchars(strip_tags($form_data->getName()));
+                $pass = htmlspecialchars(strip_tags(hash('sha512', $form_data->getPass())));
+
+                $login = $this->login($name, $pass);
+                if ($login['logged'] === true) {
+                  //  $session = new Session();
+
+                    $session->set('user_id', $login['user_id']);
+                    //todo
+                } elseif ($login['logged'] === false) {
+                    dump($login['message']);
+                }
+                dump($session->get('user_id'));
+
+            }
+
+            return $this->render('main/index.html.twig', [
+                'login_form' => $login_form->createView()
+            ]);
         }
 
-        return $this->render('main/index.html.twig', [
-            'login_form' => $login_form->createView()
-        ]);
+    }
+
+    private function login($name, $pass)
+    {
+        $user = $this->getDoctrine()->getRepository(Login::class)->findOneBy(['name' => $name, 'pass' => $pass]);
+        $message = '';
+
+        if ($user) {
+            $user_active = $user->getActive();
+
+            if ($user_active == 1) {
+                return ['logged' => true, 'user_id' => $user->getId()];
+            } elseif ($user_active == 'pending') {
+                $message = 'Váš účet není aktivní. Aktivujte ho pomocí odkazu v emailu.';
+            } elseif ($user_active == 0) {
+                $message = 'Váš účet je zablokován.';
+            }
+
+        } else {
+            $message = 'Jméno nebo heslo není správné';
+        }
+
+        return ['logged' => false, 'message' => $message];
+    }
+
+    private function logged_in()
+    {
+      //todo add logout  $session->clear();
+
     }
 }
