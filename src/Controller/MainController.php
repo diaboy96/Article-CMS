53d09a673be82efc88a68247d8d992e3d1fc8249
diaboy96 <neo6363205@gmail.com>
2019-08-24 09:25:09 +0200

    Update MainController

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+class MainController extends AbstractController //todo check all code
 {
     /**
      * @Route("/", name="main")
@@ -18,33 +18,31 @@ class MainController extends AbstractController
     {
         $session = new Session();
 
+        $login_form = $this->createForm(LoginType::class);
+
+        $login_form->handleRequest($request);
+        if ($login_form->isSubmitted() && $login_form->isValid()) {
+            $form_data = $login_form->getData();
+            $name = htmlspecialchars(strip_tags($form_data->getName()));
+            $pass = htmlspecialchars(strip_tags(hash('sha512', $form_data->getPass())));
+
+            $login = $this->login($name, $pass);
+            if ($login['logged'] === true) {
+                $session->set('user_id', $login['user_id']);
+                $session->set('user_name', $login['user_name']);
+            } elseif ($login['logged'] === false) {
+                dump($login['message']); // todo message to frontend
+            }
+
+        }
+
         $user_id = $session->get('user_id');
+
         if (isset($user_id) && !empty($user_id)){
 
             return $this->logged_in();
 
         } else {
-
-            $login_form = $this->createForm(LoginType::class);
-
-            $login_form->handleRequest($request);
-            if ($login_form->isSubmitted() && $login_form->isValid()) {
-                $form_data = $login_form->getData();
-                $name = htmlspecialchars(strip_tags($form_data->getName()));
-                $pass = htmlspecialchars(strip_tags(hash('sha512', $form_data->getPass())));
-
-                $login = $this->login($name, $pass);
-                if ($login['logged'] === true) {
-                  //  $session = new Session();
-
-                    $session->set('user_id', $login['user_id']);
-                    //todo
-                } elseif ($login['logged'] === false) {
-                    dump($login['message']);
-                }
-                dump($session->get('user_id'));
-
-            }
 
             return $this->render('main/index.html.twig', [
                 'login_form' => $login_form->createView()
@@ -62,7 +60,7 @@ class MainController extends AbstractController
             $user_active = $user->getActive();
 
             if ($user_active == 1) {
-                return ['logged' => true, 'user_id' => $user->getId()];
+                return ['logged' => true, 'user_id' => $user->getId(), 'user_name' => $user->getName()];
             } elseif ($user_active == 'pending') {
                 $message = 'Váš účet není aktivní. Aktivujte ho pomocí odkazu v emailu.';
             } elseif ($user_active == 0) {
@@ -79,6 +77,6 @@ class MainController extends AbstractController
     private function logged_in()
     {
       //todo add logout  $session->clear();
-
+        //todo add freontend render + create new template
     }
 }
