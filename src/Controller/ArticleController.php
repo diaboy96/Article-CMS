727ab8482a3ce1c261a489b12462774bcfe129b6
diaboy96 <neo6363205@gmail.com>
2019-08-30 16:04:10 +0200

    Add method fetchAllArticles which fetch all articles desc (and return them)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    /**
-     * @Route("/article/{article_id}", name="article", defaults={article_id = })
-     */
-    public function articleDetail()
+    public function fetchAllArticles()
     {
-        return $this->render('article/index.html.twig', [
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(['id' => 'DESC']);
+
+        return $articles;
+    }
+    
+    /**
+     * @Route("/article/{article_id}", name="article_detail", defaults={"article_id" = "not_set"})
+     */
+    public function articleDetail($article_id)
+    {
+
+        if ($article_id !== 'not_set') {
+
+            $article_id = intval($article_id);
+
+        }
+        return $this->render('article/article_detail.html.twig', [
             'controller_name' => 'ArticleController',
         ]);
     }
