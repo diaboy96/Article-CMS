a8e12bc3aab7d18269c4d5410aa85da5c385df39
diaboy96 <neo6363205@gmail.com>
2019-09-08 22:00:34 +0200

    Make navbar + make method for admin logout

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
@@ -12,8 +14,21 @@ class AdminController extends AbstractController
      */
     public function index()
     {
+
         return $this->render('admin/index.html.twig', [
             'admin_id' => 0
         ]);
+    }
+
+    /**
+     * @Route("/admin/logout", name="admin_logout")
+     * @return RedirectResponse
+     */
+    public function logout()
+    {
+        $session = new Session();
+        $session->clear();
+
+        return $this->redirectToRoute('admin');
     }
 }
