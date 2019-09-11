2ee828c59f18150b90714d4004881ee66651aae6
diaboy96 <neo6363205@gmail.com>
2019-09-11 11:33:11 +0200

    Adding articles work (not safe)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Form\ArticleType;
 use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -16,17 +17,44 @@ class ArticleController extends AbstractController
 {
     /**
      * @Route("/createArticle", name="article_create")
+     * @param Request $request
+     * @return RedirectResponse|Response
      */
-    public function createArticle()
+    public function createArticle(Request $request)
     {
         $admin_is_logged_in = new AdminController();
         $admin_is_logged_in->checkIfAdminIsLoggedIn();
 
         if ($admin_is_logged_in) {
-            //todo admin (returnovat admin/article_management.hmtl.twig template)
-            // data z formu odchytavat na backu (tady)
+            // make form
+            $article_form = $this->createForm(ArticleType::class);
+            $article_form->handleRequest($request);
 
-            return $this->render('admin/article_manage.html.twig');
+            if ($article_form->isSubmitted() && $article_form->isValid()) {
+                $entityManager = $this->getDoctrine()->getManager();
+                $data = $article_form->getData();
+
+                $article_header = htmlspecialchars(strip_tags($data->getHeader()));
+                $article_header = $this->cleanForXSS($article_header);
+
+                $article_content = htmlspecialchars(strip_tags($data->getContent()));
+
+
+                $article = new Article();
+                $article->setHeader($article_header);
+                $article->setContent($article_content);
+                $entityManager->persist($article);
+                $entityManager->flush();
+
+                $url = $this->generateUrl('admin', ['message' => 'Článek byl úspěšně vytvořen', 'type_message' => 'success']);
+                return $this->redirect($url.'#message');
+            }
+
+            return $this->render('admin/article_manage.html.twig', [
+                'article_form' => $article_form->createView(),
+                'article_content' => 'Obsah',
+                'article_header' => 'Nadpis'
+            ]);
         } else {
             return $this->redirectToRoute('admin');
         }
@@ -247,5 +275,43 @@ class ArticleController extends AbstractController
                 return ['is_owned_by_user' => false, 'message' => $message];
             }
         }
+    }
+
+    /**
+     * @param $data
+     * @return mixed|string|string[]|null
+     */
+    protected function cleanForXSS($data)
+    {
+// Fix &entity\n;
+        $data = str_replace(array('&amp;', '&lt;', '&gt;'), array('&amp;amp;', '&amp;lt;', '&amp;gt;'), $data);
+        $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+        $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+        $data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
+
+// Remove any attribute starting with "on" or xmlns
+        $data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
+
+// Remove javascript: and vbscript: protocols
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
+        $data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
+
+// Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
+        $data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
+
+// Remove namespaced elements (we do not need them)
+        $data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
+
+        do {
+            // Remove really unwanted tags
+            $old_data = $data;
+            $data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
+        } while ($old_data !== $data);
+
+// we are done...
+        return $data;
     }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,8 @@ class ArticleType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('header')
-            ->add('content')
+            ->add('header', TextType::class)
+            ->add('content', TextareaType::class)
         ;
     }
 
