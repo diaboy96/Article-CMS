ae783c0c2150e8489054c7d5fc3f00c2a7644637
Martin Davidík <martin.davidik@lundegaard.eu>
2023-07-07 23:20:58 +0200

    Add rector.php config file

---
+++ rector.php
@@ -0,0 +1,14 @@
+<?php
+
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+	$rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
+	$rectorConfig->sets([
+		SymfonySetList::SYMFONY_53,
+		SymfonySetList::SYMFONY_CODE_QUALITY,
+		SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
+	]);
+};
